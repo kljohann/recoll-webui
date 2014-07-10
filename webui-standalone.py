8dad227ed016bd10e6835fe835b3719c739dd8c4
Johann Klähn <kljohann@gmail.com>
2014-07-10 22:54:07 +0200

    fix launching webui-standalone from same directory
    When called like `python ./webui-standalone.py` `os.path.dirname(__file__)` returns an empty string.

--- webui-standalone.py
+++ webui-standalone.py
@@ -10,7 +10,7 @@ parser.add_argument('-p', '--port', default='8080', type=int, help='port to list
 args = parser.parse_args()
 
 # change to webui's directory and import
-os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.dirname(__file__) or '.')
 
 # set up webui and run in own http server
 webui.bottle.debug(True)
